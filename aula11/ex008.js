4d1da91a3f0b59f2bd1af26b8b7e7f5d84c2d2cd
Spartacuss-Ac <69598702+Spartacuss-Ac@users.noreply.github.com>
2020-10-10 16:34:38 -0500

    continuar

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+var vel = 80.5
+
+console.log(`Sua velocidade é de ${vel} km/h`)
+
+if (vel > 70){
+
+    console.log('Você está acima da velocidade da pista')
+
+}
+
+else{
+    console.log('Você está dentro do limite da pista')
+}
